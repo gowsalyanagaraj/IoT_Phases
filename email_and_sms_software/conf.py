9e26189d782520b519dcafb1eb71ab67a4068b33
gowsalyanagaraj <146975607+gowsalyanagaraj@users.noreply.github.com>
2023-10-31 22:39:21 +0530

    Add files via upload

---
+++ email_and_sms_software/conf.py
@@ -0,0 +1,11 @@
+# Configure Twilio API settings
+account_sid = 'ACc10811a3687ebbc48648729841790510'
+auth_token = 'd206dd7aed59ba8996cfd02cef8ed82f'
+twilio_number = '+15074874092'
+recipient_number = '+91 7506157604'
+client = Client(account_sid, auth_token)
+
+# Configure email settings
+key = 'b39fc551fa543076f27c48bcfaed0b45-b36d2969-cadd52a1'
+sandbox = 'sandbox4f2dd10cf5d3445c81a1f2f28f530684.mailgun.org'
+recipient = 'gowsalyagowsalya0712@gmail.com'
